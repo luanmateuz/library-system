345bc11dcacf6fc85c98cb4680fab8d66ea6d39b
hellowluan <hellowluan@gmail.com>
2020-06-24 19:52:58 -0300

    build: starts the entire system

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import view.LoginView;
+
+/**
+ *
+ * @author luan
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        LoginView login = new LoginView();
+    
+        login.setVisible(true);
+    }
+}
