1eca21eeef30066105036f56c206a9d54a2a32d3
hellowluan <hellowluan@gmail.com>
2020-07-04 18:41:53 -0300

    feat: book registration in the database

---
+++ src/controller/BookController.java
@@ -0,0 +1,171 @@
+package controller;
+
+import java.sql.SQLException;
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
+import model.Book;
+import model.dao.BookDAO;
+import view.BookView;
+
+/**
+ *
+ * @author luan
+ */
+public class BookController {
+
+    private BookView view;
+    private Book model;
+    private BookDAO dao;
+
+    public BookController(BookView view) {
+        this.view = view;
+    }
+
+    public void add() {
+        if (view.checksData()) {
+
+            model = new Book();
+
+            model.setTitle(view.getTxtTitle().getText());
+            model.setAuthor(view.getTxtAuthor().getText());
+            model.setEdition(view.getTxtEdition().getText());
+            model.setYear(view.getTxtYear().getText());
+            model.setAvailable(view.getCmbAvailable().getSelectedItem().toString());
+
+            try {
+                dao = new BookDAO();
+
+                dao.insert(model);
+
+                view.showMessage("Registered Successfully");
+                view.cleanFields();
+                view.disableFields();
+                
+                this.showList();
+
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+                view.showMessage("Error when registering Student");
+            }
+
+        } else {
+            view.showMessage("Fill all Data");
+        }
+    }
+
+    public void alter() {
+        if (view.checksData()) {
+
+            if (view.showConfirm("Confirm change of registration?")) {
+                model = new Book();
+
+                model.setId(Integer.parseInt(view.getTxtId().getText()));
+                model.setTitle(view.getTxtTitle().getText());
+                model.setAuthor(view.getTxtAuthor().getText());
+                model.setEdition(view.getTxtEdition().getText());
+                model.setYear(view.getTxtYear().getText());
+                model.setAvailable(view.getCmbAvailable().getSelectedItem().toString());
+
+                try {
+                    dao = new BookDAO();
+
+                    dao.update(model);
+
+                    view.showMessage("Altered Successfully");
+                    view.cleanFields();
+                    view.disableFields();
+
+                    this.showList();
+
+                } catch (SQLException e) {
+                    System.out.println(e.getMessage());
+                    view.showMessage("Error when registering Book");
+                }
+            }
+        } else {
+            view.showMessage("Fill all Data");
+        }
+    }
+
+    public void delete() {
+        if (view.showConfirm("Delete registration?")) {
+
+            model = new Book();
+
+            model.setId(Integer.parseInt(view.getTxtId().getText()));
+
+            try {
+                dao = new BookDAO();
+
+                dao.delete(model.getId());
+
+                view.showMessage("Deleted Successfully");
+                view.cleanFields();
+                view.disableFields();
+
+                this.showList();
+
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+                view.showMessage("Error when delete Book");
+            }
+
+        }
+    }
+
+    public void fillInData() {
+
+        int selectedLine = view.getTblSearch().getSelectedRow();
+
+        if (selectedLine != -1) {
+
+            view.fillsFields(
+                    view.getTblSearch().getValueAt(selectedLine, 0).hashCode(),
+                    view.getTblSearch().getValueAt(selectedLine, 1).toString(),
+                    view.getTblSearch().getValueAt(selectedLine, 2).toString(),
+                    view.getTblSearch().getValueAt(selectedLine, 3).toString(),
+                    view.getTblSearch().getValueAt(selectedLine, 4).toString(),
+                    view.getTblSearch().getValueAt(selectedLine, 5).toString()
+            );
+        }
+
+        view.enableFields();
+    }
+
+    public void showList() {
+        List<Book> book;
+
+        try {
+            dao = new BookDAO();
+
+            String title = "%" + view.getTxtTitleSearch().getText() + "%";
+
+            book = dao.select(title);
+
+            if (!book.isEmpty()) {
+
+                String[] line = new String[]{null, null, null, null, null, null};
+
+                DefaultTableModel data = (DefaultTableModel) view.getTblSearch().getModel();
+                data.setNumRows(0);
+
+                for (int i = 0; i < book.size(); i++) {
+                    data.addRow(line);
+                    data.setValueAt(book.get(i).getId(), i, 0);
+                    data.setValueAt(book.get(i).getTitle(), i, 1);
+                    data.setValueAt(book.get(i).getAuthor(), i, 2);
+                    data.setValueAt(book.get(i).getEdition(), i, 3);
+                    data.setValueAt(book.get(i).getYear(), i, 4);
+                    data.setValueAt(book.get(i).getAvailable(), i, 5);
+                }
+
+            } else {
+                view.showMessage("No Record Found");
+            }
+
+        } catch (SQLException e) {
+            view.showMessage("Error when list Student");
+        }
+
+    }
+}
---
+++ src/model/dao/BookDAO.java
@@ -0,0 +1,99 @@
+package model.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import model.Book;
+
+/**
+ *
+ * @author luan
+ */
+public class BookDAO {
+
+    private Connection connection;
+
+    public BookDAO() throws SQLException {
+        this.connection = ConnectionDAO.getConnection();
+    }
+    
+    public void insert(Book book) throws SQLException {
+        
+        String sql = "INSERT INTO book (title, author, edition, year, available) "
+                + "VALUES (?, ?, ?, ?, ?)";
+        
+        PreparedStatement stmt = connection.prepareCall(sql);
+        
+        stmt.setString(1, book.getTitle());
+        stmt.setString(2, book.getAuthor());
+        stmt.setString(3, book.getEdition());
+        stmt.setString(4, book.getYear());
+        stmt.setString(5, book.getAvailable());
+        
+        stmt.execute();
+        stmt.close();
+    }
+    
+    public void update(Book book) throws SQLException {
+        
+        String sql = "UPDATE book SET title=?, author=?, edition=?, year=?, available=? WHERE id=?";
+        
+        PreparedStatement stmt = connection.prepareCall(sql);
+        
+        stmt.setInt(6, book.getId());
+        stmt.setString(1, book.getTitle());
+        stmt.setString(2, book.getAuthor());
+        stmt.setString(3, book.getEdition());
+        stmt.setString(4, book.getYear());
+        stmt.setString(5, book.getAvailable());
+        
+        stmt.execute();
+        stmt.close();
+    }
+    
+    public void delete(int id) throws SQLException {
+        
+        String sql = "DELETE FROM book WHERE id=?";
+        
+        PreparedStatement stmt = connection.prepareCall(sql);
+        
+        stmt.setInt(1, id);
+        
+        stmt.execute();
+        stmt.close();
+    } 
+    
+    public List<Book> select(String title) throws SQLException {
+    
+        String sql = "SELECT * FROM book WHERE title LIKE ?";
+        PreparedStatement stmt = connection.prepareCall(sql);
+            
+        stmt.setString(1, title);
+        
+        ResultSet rs = stmt.executeQuery();
+        
+        List<Book> list = new ArrayList<>();
+        
+        while (rs.next()) {
+            Book book = new Book();
+            
+            book.setId(rs.getInt("id"));
+            book.setTitle(rs.getString("title"));
+            book.setAuthor(rs.getString("author"));
+            book.setEdition(rs.getString("edition"));
+            book.setYear(rs.getString("year"));
+            book.setAvailable(rs.getString("available"));
+            
+            list.add(book);
+        }
+        
+        rs.close();
+        stmt.close();
+        
+        return list;
+        
+    }
+}
