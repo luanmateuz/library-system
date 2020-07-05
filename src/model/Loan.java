ed43a3aaadf42eb36e3229d7fe5303891ae1ecd2
hellowluan <hellowluan@gmail.com>
2020-07-05 17:46:19 -0300

    feat: loan model created

---
+++ src/model/Loan.java
@@ -0,0 +1,59 @@
+package model;
+
+/**
+ *
+ * @author luan
+ */
+public class Loan {
+    
+    private int id;
+    private int idStudent;
+    private int idBook;
+    private String loanDate;
+    private String returnDate;
+
+    public Loan() {
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdStudent() {
+        return idStudent;
+    }
+
+    public void setIdStudent(int idStudent) {
+        this.idStudent = idStudent;
+    }
+
+    public int getIdBook() {
+        return idBook;
+    }
+
+    public void setIdBook(int idBook) {
+        this.idBook = idBook;
+    }
+
+    public String getLoanDate() {
+        return loanDate;
+    }
+
+    public void setLoanDate(String loanDate) {
+        this.loanDate = loanDate;
+    }
+
+    public String getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(String returnDate) {
+        this.returnDate = returnDate;
+    }
+    
+}
